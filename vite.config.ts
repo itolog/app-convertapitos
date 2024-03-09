f0c131d9e1bd4fb1db8566cea6671cf3c56d7b15
itolog <paukanlove@gmail.com>
2024-03-09 16:03:45 +0200

    config linters

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+// @ts-ignore
+import oxlintPlugin from 'vite-plugin-oxlint'
+// @ts-ignore
+import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        oxlintPlugin({
+            path: 'src',
+        }),
+        eslintPlugin()],
 })
