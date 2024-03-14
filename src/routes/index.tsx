08d804166fb0c537a93ace916ccbf5d77dace07d
itolog <paukanlove@gmail.com>
2024-03-14 20:21:49 +0200

    added plugins

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,18 @@ import { createFileRoute } from "@tanstack/react-router";
 import FileUpload from "@/shared/components/FileUpload/FileUpload.tsx";
 import useFileUpload from "@/shared/components/FileUpload/hooks/useFileUpload.tsx";
 
+import { ACCEPTED_IMAGE_TYPES } from "@/constants/fileUploadConstants.ts";
+
 const Index = () => {
   const { files, onUpdateFiles } = useFileUpload();
 
   return (
     <div>
-      <FileUpload files={files} onupdatefiles={onUpdateFiles} />
+      <FileUpload
+        acceptedFileTypes={ACCEPTED_IMAGE_TYPES}
+        files={files}
+        onupdatefiles={onUpdateFiles}
+      />
     </div>
   );
 };
--- src/shared/components/FileUpload/FileUpload.tsx
+++ src/shared/components/FileUpload/FileUpload.tsx
@@ -2,12 +2,22 @@ import { FC } from "react";
 import { FilePond, FilePondProps, registerPlugin } from "react-filepond";
 
 import cl from "classnames";
+import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+// Import the plugin styles
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 import "filepond/dist/filepond.min.css";
 
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+registerPlugin(
+  FilePondPluginImageExifOrientation,
+  FilePondPluginImagePreview,
+  FilePondPluginFileValidateType,
+  FilePondPluginFileValidateSize,
+  FilePondPluginImagePreview,
+);
 
 interface Classes {
   root?: string;
