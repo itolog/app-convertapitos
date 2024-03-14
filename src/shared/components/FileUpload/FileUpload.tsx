9c8610f66b3868ab77b4d22ecbc56836eae359d5
itolog <paukanlove@gmail.com>
2024-03-14 20:23:11 +0200

    removed duplicates

--- src/shared/components/FileUpload/FileUpload.tsx
+++ src/shared/components/FileUpload/FileUpload.tsx
@@ -6,8 +6,6 @@ import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
 import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
-// Import the plugin styles
-import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 import "filepond/dist/filepond.min.css";
 
