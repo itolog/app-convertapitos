2a57ea6aecdfdb24361a28f449c62d0e2436cccd
itolog <paukanlove@gmail.com>
2024-03-20 17:29:20 +0200

    added icon to lang switcher

--- src/components/Inputs/CoAutocomplete/CoAutocomplete.tsx
+++ src/components/Inputs/CoAutocomplete/CoAutocomplete.tsx
@@ -1,32 +1,38 @@
+import * as React from "react";
+
 import Autocomplete from "@mui/material/Autocomplete";
-import { AutocompleteProps } from "@mui/material/Autocomplete/Autocomplete";
+import {
+	AutocompleteProps,
+	AutocompleteRenderInputParams,
+} from "@mui/material/Autocomplete/Autocomplete";
 import TextField from "@mui/material/TextField";
 
 interface CoAutocompleteProps<T> extends Omit<AutocompleteProps<T, any, any, any>, "renderInput"> {
 	label?: string;
 	placeholder?: string;
+	customRenderInput?: (params: AutocompleteRenderInputParams) => React.ReactNode;
 }
 
 const CoAutocomplete = <T extends object>({
 	label,
 	placeholder,
+	customRenderInput,
 	...props
 }: CoAutocompleteProps<T>) => {
-	return (
-		<Autocomplete
-			{...props}
-			renderInput={(params) => (
-				<TextField
-					{...params}
-					label={label}
-					placeholder={placeholder}
-					inputProps={{
-						...params.inputProps,
-						autoComplete: "new-password",
-					}}
-				/>
-			)}
+	const renderInput = (params: AutocompleteRenderInputParams) => (
+		<TextField
+			{...params}
+			label={label}
+			placeholder={placeholder}
+			inputProps={{
+				...params.inputProps,
+				autoComplete: "new-password",
+			}}
 		/>
+	);
+
+	return (
+		<Autocomplete {...props} renderInput={customRenderInput ? customRenderInput : renderInput} />
 	);
 };
 
--- src/components/Inputs/LangSwitcher/LangSwitcher.tsx
+++ src/components/Inputs/LangSwitcher/LangSwitcher.tsx
@@ -1,7 +1,10 @@
 import { FC, SyntheticEvent, useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
 
+import { createFilterOptions, InputAdornment } from "@mui/material";
+import { AutocompleteRenderInputParams } from "@mui/material/Autocomplete/Autocomplete";
 import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 import CoAutocomplete from "@/components/Inputs/CoAutocomplete/CoAutocomplete.tsx";
 import useOptions, { Option } from "@/components/Inputs/LangSwitcher/hooks/useOptions.tsx";
@@ -24,12 +27,47 @@ const LangSwitcher: FC<LangSwitcherProps> = ({ width = 200 }) => {
 			value: (string | Option)[] | NonNullable<string | Option> | null,
 		) => {
 			const val = value as Option;
-			await i18n.changeLanguage(val.value);
+			if (val) {
+				await i18n.changeLanguage(val.value);
 
-			setValue(val);
+				setValue(val);
+			}
 		},
 		[i18n],
 	);
+
+	const renderInput = (params: AutocompleteRenderInputParams) => {
+		const option: Option = optionsNormalized[i18n.language];
+		return (
+			<TextField
+				{...params}
+				placeholder={"placeholder"}
+				inputProps={{
+					...params.inputProps,
+					autoComplete: "new-password",
+				}}
+				InputProps={{
+					...params.InputProps,
+					startAdornment: (
+						<InputAdornment position="start">
+							<img
+								loading="lazy"
+								width="20"
+								srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+								src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+								alt=""
+							/>
+							{}
+						</InputAdornment>
+					),
+				}}
+			/>
+		);
+	};
+
+	const filterOptions = createFilterOptions({
+		stringify: (option: Option) => option.code + option.label,
+	});
 
 	return (
 		<CoAutocomplete<Option>
@@ -45,8 +83,10 @@ const LangSwitcher: FC<LangSwitcherProps> = ({ width = 200 }) => {
 			sx={{ width }}
 			isOptionEqualToValue={(option, value) => option.value === value.value}
 			getOptionLabel={(option) => {
-				return (option as Option).label;
+				return (option as Option).code;
 			}}
+			customRenderInput={renderInput}
+			filterOptions={filterOptions}
 			renderOption={(props, option) => (
 				<Box component="li" sx={{ "& > img": { mr: 2, flexShrink: 0 } }} {...props}>
 					<img
