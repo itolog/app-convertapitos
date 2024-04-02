7e36e663e2a29c3bf6ae5724563ab6ad88d4f2fa
itolog <paukanlove@gmail.com>
2024-04-02 13:59:37 +0300

    added favicon

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,12 @@ import { defineConfig } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 // @ts-ignore
 import oxlintPlugin from "vite-plugin-oxlint";
+import { VitePWA } from "vite-plugin-pwa";
 import stylelint from "vite-plugin-stylelint";
 import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	build: {
-		manifest: true,
-	},
 	plugins: [
 		react(),
 		oxlintPlugin({
@@ -30,6 +28,49 @@ export default defineConfig({
 		webfontDownload([
 			"https://fonts.googleapis.com/css2?family=Vollkorn:wght@400;500;700&display=swap",
 		]),
+		VitePWA({
+			manifest: {
+				display: "standalone",
+				theme_color: "#0f172a",
+				background_color: "#1e293b",
+				scope: "/",
+				start_url: "/",
+				name: "ConvertApiTos",
+				short_name: "CAT",
+				icons: [
+					{
+						src: "/icon-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/icon-256x256.png",
+						sizes: "256x256",
+						type: "image/png",
+					},
+					{
+						src: "/icon-384x384.png",
+						sizes: "384x384",
+						type: "image/png",
+					},
+					{
+						src: "/icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
 	],
 	resolve: {
 		alias: {
