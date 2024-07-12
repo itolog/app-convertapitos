9279ee201829e8d27d8c2a13f04d6bfa8f14257a
itolog <paukanlove@gmail.com>
2024-07-12 14:09:45 +0300

    update env

--- src/store/services/Image/index.ts
+++ src/store/services/Image/index.ts
@@ -6,9 +6,11 @@ import { ApiResponse } from "@/types";
 
 import { DataResponse } from "@/store/services/Image/types.ts";
 
+const baseUrl = `${import.meta.env.VITE_API_BASE_URL}/${import.meta.env.VITE_API_VERSION}`;
+
 export const imageApi = createApi({
 	reducerPath: "image",
-	baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
+	baseQuery: fetchBaseQuery({ baseUrl }),
 	endpoints: (build) => ({
 		convertImage: build.mutation<ApiResponse<DataResponse>, Partial<FormValues>>({
 			query(body) {
