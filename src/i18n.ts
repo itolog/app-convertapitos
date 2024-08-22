1e374de1c5c6b677dee27945a0717aa6dd96b726
itolog <paukanlove@gmail.com>
2024-08-22 18:48:47 +0300

    sign in page(prettier)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,6 @@
-import { getUserLocale } from "@/services/locale";
 import { getRequestConfig } from "next-intl/server";
+
+import { getUserLocale } from "@/services/locale";
 
 export default getRequestConfig(async () => {
 	const locale = await getUserLocale();
