fda5566db2f7dee288b8cc46ddfa9df102a90f3e
itolog <paukanlove@gmail.com>
2024-10-01 15:04:11 +0300

    fix singnin page layout

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -9,7 +9,7 @@ import CoText from "@/components/ui/CoText/CoText";
 
 const Page = () => {
 	return (
-		<div className={"flex pt-20 justify-center h-full"}>
+		<div className={"flex pt-20 justify-center"}>
 			<CoCard>
 				<div className={"flex flex-col p-4 gap-4"}>
 					<CoText className="text-xl md:text-3xl" textProps={{ target: "Auth" }}>
