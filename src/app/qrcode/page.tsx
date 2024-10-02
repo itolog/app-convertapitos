9c6405bcd92dc2bf892373fca0cb09880268d2e4
itolog <paukanlove@gmail.com>
2024-10-02 10:32:16 +0300

    SEO

--- src/app/qrcode/page.tsx
+++ src/app/qrcode/page.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
+import type { Metadata } from "next";
+
 import DevPlaceholder from "@/components/ui/DevPlaceholder/DevPlaceholder";
+
+export const metadata: Metadata = {
+	title: "Qrcode generation",
+	description: "Generation and customization of qrcode",
+	keywords: ["qrcode generation", "qrcode customization", "qrcode"],
+};
 
 const Page = () => {
 	return (
--- src/app/text/page.tsx
+++ src/app/text/page.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
+import type { Metadata } from "next";
+
 import DevPlaceholder from "@/components/ui/DevPlaceholder/DevPlaceholder";
+
+export const metadata: Metadata = {
+	title: "Text to speech",
+	description:
+		"Преобразование текста в речь, Text to speech. Converter text to voice with natural sounding voices. Free mp3 download.",
+	keywords: ["text to speech", "tts", "converter text to voice"],
+};
 
 const Page = () => {
 	return (
