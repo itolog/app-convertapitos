196e498e5261fb68aab697fc49e7440e9a2ff6eb
itolog <paukanlove@gmail.com>
2024-10-03 11:55:58 +0300

    add QrCode component

--- src/app/text/page.tsx
+++ src/app/text/page.tsx
@@ -5,18 +5,18 @@ import type { Metadata } from "next";
 import DevPlaceholder from "@/components/ui/DevPlaceholder/DevPlaceholder";
 
 export const metadata: Metadata = {
-	title: "Text to speech",
-	description:
-		"Преобразование текста в речь, Text to speech. Converter text to voice with natural sounding voices. Free mp3 download.",
-	keywords: ["text to speech", "tts", "converter text to voice"],
+  title: "Text to speech",
+  description:
+    "Преобразование текста в речь, Text to speech. Converter text to voice with natural sounding voices. Free mp3 download.",
+  keywords: ["text to speech", "tts", "converter text to voice"],
 };
 
 const Page = () => {
-	return (
-		<div className={"flex items-center justify-center"}>
-			<DevPlaceholder />
-		</div>
-	);
+  return (
+    <div className={"flex items-center justify-center"}>
+      <DevPlaceholder />
+    </div>
+  );
 };
 
 export default Page;
