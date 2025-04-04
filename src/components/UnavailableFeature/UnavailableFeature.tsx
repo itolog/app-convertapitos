038c8c7f59422b3a9762948b5852095c15bb5ef3
itolog <paukanlove@gmail.com>
2025-04-04 15:46:50 +0300

    added simple UnavailableFeature card

--- src/components/UnavailableFeature/UnavailableFeature.tsx
+++ src/components/UnavailableFeature/UnavailableFeature.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import CoCard from "@/components/common/Cards/CoCard/CoCard";
+
 const UnavailableFeature = () => {
-  return <div>dis</div>;
+  return (
+    <CoCard>
+      <div className={"flex h-full items-center p-3 text-xl md:text-2xl"}>
+        The feature is not available
+      </div>
+    </CoCard>
+  );
 };
 
 export default UnavailableFeature;
