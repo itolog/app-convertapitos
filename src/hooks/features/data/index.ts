ec7a016558694b201de8b06345260e0c3473df42
itolog <paukanlove@gmail.com>
2025-05-05 18:35:24 +0300

    add vcard(enable)

--- src/hooks/features/data/index.ts
+++ src/hooks/features/data/index.ts
@@ -2,5 +2,4 @@ import { FEATURE, FeatureItemsMap } from "@/types/features";
 
 export const disabledFeatures: Partial<FeatureItemsMap> = {
   [FEATURE.CONVERT]: false,
-  [FEATURE.QRCODE_VCARD]: false,
 };
