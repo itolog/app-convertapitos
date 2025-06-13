0c3961d3c2e093d8b898bc53cbc88525577e56b8
itolog <paukanlove@gmail.com>
2025-06-13 12:52:14 +0300

    add loading state

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import AppSpinner from "@/components/common/Loaders/AppSpinner/AppSpinner";
+
+const Loading = () => {
+  return <AppSpinner />;
+};
+
+export default Loading;
